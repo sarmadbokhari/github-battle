cd715936103b636add72ac43d611d6bc04fca654
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2017-04-20 16:20:29 -0700

    Setup Results component and add to App

--- app/components/App.js
+++ app/components/App.js
@@ -8,6 +8,7 @@ var Navbar = require('./Navbar');
 var Home = require('./Home');
 var Battle = require('./Battle')
 var Popular = require('./Popular');
+var Results = require('./Results');
 
 class App extends React.Component {
   render() {
@@ -16,9 +17,10 @@ class App extends React.Component {
          <div className='container'>
            <Navbar />
            <Switch>
-             <Route exact path='/'        component={Home} />
-             <Route exact path='/battle'  component={Battle} />
-             <Route path='/popular'       component={Popular} />
+             <Route exact path='/'         component={Home} />
+             <Route exact path='/battle'   component={Battle} />
+             <Route path='/battle/results' component={Results} />
+             <Route path='/popular'        component={Popular} />
              <Route render={() => (<p>404 Not Found</p>)} />
            </Switch>
          </div>
