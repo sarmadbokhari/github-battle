12eb20647a0df7b93da72e5ab7a01078199b0b50
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2017-04-19 17:57:17 -0700

    Add api to Popular, life cycle componentDidMount to fetch All popular language on component load

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var api = require('../utils/api');
 
 // Stateless Functional Component
 function SelectLanguage(props) {
@@ -29,10 +30,19 @@ class Popular extends React.Component {
 		super(props);
 
 		this.state = {
-			selectedLanguage: 'All'
+			selectedLanguage: 'All',
+			repos: null
 		}
 
 		this.updateLanguage = this.updateLanguage.bind(this);
+	}
+
+	componentDidMount() {
+		// AJAX request
+		api.fetchPopularRepos(this.state.selectedLanguage)
+			.then(function(repos) {
+				console.log(repos);
+			})
 	}
 
 	updateLanguage(language) {
