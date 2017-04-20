f973488434c2bb8c056a472727bb37cf2e6fb1bd
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2017-04-20 13:17:35 -0700

    Add Home component

---
+++ app/components/Home.js
@@ -0,0 +1,13 @@
+var React = require('react');
+var Link = require('react-router-dom').Link;
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h1>Github Battle: Battle your friends...</h1>
+      <Link className="button" to="/battle">Battle</Link>
+    </div>
+  )
+}
+
+module.exports = Home;
