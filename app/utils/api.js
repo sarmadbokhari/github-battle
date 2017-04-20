2144b9d5e9b83d811b1f9fea2cb7ebb1fdf3e2db
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2017-04-20 16:38:55 -0700

    Add github battle api methods

--- app/utils/api.js
+++ app/utils/api.js
@@ -4,6 +4,56 @@ var id = 'GITHUB_CLIENT_ID';
 var secret = 'SECRET_ID';
 var params = `?client_id=${id}&client_secret${secret}`;
 
+function getProfile(username) {
+  return axios.get('https://github.com/users/' + username)
+    .then(function(user) {
+      return user.data;
+    });
+}
+
+function getRepos(username) {
+  return axios.get('https://github.com/users/' + username + '/repos/&per_page=100')
+}
+
+function getStarCount(repos) {
+  return repos.data.reduce(function(count, repo) {
+    return count + repo.stargazers_count;
+  }, 0)
+}
+
+function calculateScore(profile, repos) {
+  var followers = profile.followers;
+  var totalStars = getStarCount(repos);
+
+  return (followers * 3) + totalStars;
+}
+
+function handleError(err) {
+  console.warn(err);
+  return null;
+}
+
+function getUserData(player) {
+  return axios.all([
+    getProfile(player),
+    getRepos(player)
+  ]).then(function(data) {
+    var profile = data[0];
+    var repos = data[1];
+
+    return {
+      profile: profile,
+      score: calculateScore(profile, repos)
+    }
+  });
+}
+
+function sortPlayers(players) {
+  return players.sort(function(a,b) {
+    return b.score - a.score;
+  });
+}
+
 module.exports = {
   fetchPopularRepos: function(language) {
     var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + '&sort=stars&order=desc&type=Repositories');
