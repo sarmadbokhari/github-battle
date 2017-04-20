ed330d82a4d58d7e8ac705d4223b910582e9a41d
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2017-04-20 13:33:14 -0700

    Add Battle component, add react router Switch, add webpack devServer to prevent refresh from fetching assets from server

--- app/components/App.js
+++ app/components/App.js
@@ -2,9 +2,11 @@ var React = require('react');
 var ReactRouter = require('react-router-dom');
 var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
 
 var Navbar = require('./Navbar');
 var Home = require('./Home');
+var Battle = require('./Battle')
 var Popular = require('./Popular');
 
 class App extends React.Component {
@@ -13,8 +15,12 @@ class App extends React.Component {
       <Router>
          <div className='container'>
            <Navbar />
-           <Route path='/' exact  component={Home} />
-           <Route path='/popular' component={Popular} />
+           <Switch>
+             <Route exact path='/'        component={Home} />
+             <Route exact path='/battle'  component={Battle} />
+             <Route path='/popular'       component={Popular} />
+             <Route render={() => (<p>404 Not Found</p>)} />
+           </Switch>
          </div>
        </Router>
     )
