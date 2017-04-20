fad4e60d116b8a1c00efb40788b809d9157ea83e
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2017-04-20 16:23:49 -0700

    Add functionality for github client and secret id to params

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,5 +1,9 @@
 var axios = require('axios');
 
+var id = 'GITHUB_CLIENT_ID';
+var secret = 'SECRET_ID';
+var params = `?client_id=${id}&client_secret${secret}`;
+
 module.exports = {
   fetchPopularRepos: function(language) {
     var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + '&sort=stars&order=desc&type=Repositories');
